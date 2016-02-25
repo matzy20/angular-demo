10401b84f7720e95770e172d86c9a24e6a9cfb26
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-24 19:39:34 -1000

    got angular setup in html

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var path = require('path');
-var morgan = require('morgan');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use(morgan('dev'));
-app.use(methodOverride('_method'));
 app.use(express.static(path.resolve(__dirname, 'Public')));
 
 
