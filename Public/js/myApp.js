c13c36533274116f03d7636d5c1796abfbf9ec0f
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-24 20:44:36 -1000

    step angular module

---
+++ Public/js/myApp.js
@@ -0,0 +1,13 @@
+//you would put other dependencies you're dependent on in []
+angular.module('myApp',[]);
+
+var myApp = angular.module('myApp');
+
+myApp
+  .config(function () {
+    //config
+  })
+  .run(function () {
+    //initializing
+
+  });
