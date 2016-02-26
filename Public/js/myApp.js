e52a2a661a75b81db81074ac8e3d523c1750b60d
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-25 21:39:43 -1000

    routing

--- Public/js/myApp.js
+++ Public/js/myApp.js
@@ -1,18 +1,25 @@
 //you would put other dependencies you're dependent on in []
-angular.module('myApp',[]);
+angular.module('myApp',['ngRoute']);
 
 var myApp = angular.module('myApp');
 
 myApp
-  .config(function (MoviesProvider) {
+  .config(function ($routeProvider, MoviesProvider) {
     //config
     MoviesProvider.setEndpoint('/api');
+
+    //Routes
+    $routeProvider
+      .when('/404', {
+        templateUrl: '/templates/404.html'
+      })
+      .otherwise('/404');
   })
   .run([
     '$rootScope',
     'APP_VERSION',
     function ($rootScope, APP_VERSION) {
-      //initializing
+      //initializings
       console.log("Start!");
       $rootScope.version = APP_VERSION;
     }
